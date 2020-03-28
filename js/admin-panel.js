c28e9452c874278a66aeba8f6643f5902166f6fd
oguzbakir <b21627007@cs.hacettepe.edu.tr>
2020-03-28 17:45:17 +0300

    admin panel product add pages added

--- js/admin-panel.js
+++ js/admin-panel.js
@@ -1,9 +1,35 @@
-var e = document.getElementById("book_type");
+$('#book_type').change(function (){
+    var index = $(this).prop('selectedIndex');
 
-if (e.selectedIndex == 1) {
-    
-} else if (e.selectedIndex == 2) {
+    if (index == 1) {
+        console.log("hede");
+        clearOthers();
+        $('#book').removeClass('d-none');
+    } else if (index == 2) {
+        clearOthers();
+        $('#e-book').removeClass('d-none');
+    } else if (index == 3) {
+        clearOthers();
+        $('#audiobook').removeClass('d-none');
+    } else if (index == 4) {
+        clearOthers();
+        $('#e-book_reader').removeClass('d-none');
+    } else if (index == 5) {
+        clearOthers();
+        $('#e-book_reader_case').removeClass('d-none');
+    } else if (index == 6) {
+        clearOthers();
+        $('#book_case').removeClass('d-none');
+    }
+})
 
-} else if (e.selectedIndex == 3) {
-    
+
+
+function clearOthers() {
+    $('#book').addClass('d-none');
+    $('#e-book').addClass('d-none');
+    $('#audiobook').addClass('d-none');
+    $('#e-book_reader').addClass('d-none');
+    $('#e-book_reader_case').addClass('d-none');
+    $('#book_case').addClass('d-none');
 }
